788e4ebdbe0b9a38a6bfb8ec940f5d8da3ad19c7
Ntarekp <kpntare@gmail.com>
2025-09-22 11:32:13 +0200

    Refactor list initialization and improve sorting functionality

--- 1.project/list_manapilataion.py
+++ 1.project/list_manapilataion.py
@@ -1,14 +1,26 @@
-list= ["Prince","is","The","best","Candidate","Ever", "who", "is", "going","win", "for","president"]
+list = [
+    "Prince",
+    "is",
+    "The",
+    "best",
+    "Candidate",
+    "Ever",
+    "who",
+    "is",
+    "going",
+    "win",
+    "for",
+    "president",
+]
 
-#list[0] = ["NTARE","KAYITARE"] #it likely gives a complex dsa
+# list[0] = ["NTARE","KAYITARE"] #it likely gives a complex dsa
 print(list)
 
-#inserting an element in a list
+# inserting an element in a list
 
 list.insert(2, "anelegant")
 
 print(list)
-
 
 
 print(list[-20:-14])
@@ -21,21 +33,10 @@ list.sort(key=len)
 print(list)
 
 
+def myFunc(e):
+    return len(e) >4
+list.sort(key=myFunc)
+print(list)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+filtered_list = [item for item in list if len(item) > 4]
+print(filtered_list)
